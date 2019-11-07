eeb7a9f90450e244d4f5c3d8204fc29ae5506cd6
Taieb <mohamedtaieb.alfalah@esprit.tn>
2019-11-07 19:50:48 +0000

    update

--- install.sh
+++ install.sh
@@ -29,9 +29,6 @@ cd /home/nick/tools
 
 
 
-#############BASICS############
-
-
 # spotify
 curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
