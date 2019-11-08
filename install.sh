a9426107bf70d989e387cfc7ed32ac5577fd806d
Taieb <mohamedtaieb.alfalah@esprit.tn>
2019-11-08 06:35:01 +0100

    update

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ apt install -y libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb \
     libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev \
     libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev \
     libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev \
-    libxcb-xrm0 libxcb-xrm-dev autoconf
+    libxcb-xrm0 libxcb-xrm-dev autoconf ffmpeg dmenu dunst
 cd /tmp
 git clone https://www.github.com/Airblader/i3 i3-gaps
 cd /tmp/i3-gaps
