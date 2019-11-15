6ba1829dc93c23313fe24feb6ae0943dbaac3aa9
Taieb <mohamedtaieb.alfalah@esprit.tn>
2019-11-15 20:21:57 +0000

    change apt to apt-get
    fix #1

--- install.sh
+++ install.sh
@@ -2,15 +2,17 @@
 
 echo "*** OK, let's go ! ***"
 
-sudo apt install curl git keepassxc transmission-qt firefox-esr rofi neofetch 
+sudo apt-get install curl git keepassxc transmission-qt firefox-esr rofi neofetch 
 scrot feh compton zathura lxappearance wicd wicd-gtk compton kitty
 
 
-apt install -y libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb \
+apt-get install -y libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb \
     libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev \
     libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev \
     libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev \
     libxcb-xrm0 libxcb-xrm-dev autoconf ffmpeg dmenu dunst
+    
+    
 cd /tmp
 git clone https://www.github.com/Airblader/i3 i3-gaps
 cd /tmp/i3-gaps
