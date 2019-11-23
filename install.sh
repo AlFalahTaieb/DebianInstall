ab7394d3dab05d4f6bd78258ee672f36546cd24d
Taieb <mohamedtaieb.alfalah@esprit.tn>
2019-11-23 16:47:33 +0100

    update with message

--- install.sh
+++ install.sh
@@ -1,22 +1,28 @@
 #############BASICS#############
 
 echo "*** OK, let's go ! ***"
-
 [ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
+echo "**Installing, Packages***"
+apt-get install curl git keepassxc transmission-qt firefox-esr rofi neofetch \
+scrot feh compton zathura lxappearance wicd wicd-gtk kitty pulseaudio pavucontrol \
+jq xbacklight rofi cmus ffmpeg dmenu dunst zeal p7zip
 
- apt-get install curl git keepassxc transmission-qt firefox-esr rofi neofetch 
-scrot feh compton zathura lxappearance wicd wicd-gtk kitty pulseaudio pavucontrol 
+echo "** SSD Optimization **"
+## SSD Optimization  https://wiki.debian.org/SSDOptimization 
+cp /usr/share/doc/util-linux/examples/fstrim.{service,timer} /etc/systemd/system
+systemctl enable fstrim.timer
+systemctl start fstrim.timer
 
-
+echo "** Packages related to i3-gaps **"
 ## Before Installing i3-gaps
 apt-get install -y libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb \
     libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev \
     libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev \
     libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev \
     libxcb-xrm0 libxcb-xrm-dev autoconf  xutils-dev libtool automake  libxcb-shape0-dev \
-    jq xbacklight rofi cmus ffmpeg dmenu dunst 
-    
 
+    
+echo "** Installing i3-gaps **"
 ## install i3-gaps 
 cd tmp
 git clone https://www.github.com/Airblader/i3 i3-gaps
@@ -31,22 +37,19 @@ make
 sudo make install
 
 
-
-
-
-
+echo "** Installing Spotify **"
 # spotify
 curl -sS https://download.spotify.com/debian/pubkey.gpg |  apt-key add -
 echo deb http://repository.spotify.com stable non-free |  tee /etc/apt/sources.list.d/spotify.list
 apt-get update
 apt-get install spotify-client
 
-# Papiru Icon
-
+echo "** Installing Papirus Icons **"
+# Papirus Icon
 add-apt-repository ppa:papirus/papirus
-echo "Installing Papirus Icons"
 apt install papirus-icon-theme -yy
 
+echo "** Fixing touchpads **"
 # Touchpad settings
 tee /usr/share/X11/xorg.conf.d/41-libinput-custom.conf <<EOF
 Section "InputClass"
@@ -58,7 +61,10 @@ Section "InputClass"
 EndSection
 EOF
 
+echo '** Updating Grub **'
 # Grub update for boot load screen
 sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash pci=nomsi,noaer"/' /etc/default/grub
 update-grub
 
+sleep 5
+reboot
