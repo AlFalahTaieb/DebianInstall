ab42e5694a1ea1ab6dc6f7964a583f3bf247e395
Taieb <mohamedtaieb.alfalah@esprit.tn>
2019-11-23 14:58:45 +0100

    update i3-gaps

--- install.sh
+++ install.sh
@@ -7,26 +7,28 @@ echo "*** OK, let's go ! ***"
  apt-get install curl git keepassxc transmission-qt firefox-esr rofi neofetch 
 scrot feh compton zathura lxappearance wicd wicd-gtk kitty pulseaudio pavucontrol 
 
+
+## Before Installing i3-gaps
 apt-get install -y libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb \
     libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev \
     libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev \
     libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev \
-    libxcb-xrm0 libxcb-xrm-dev autoconf ffmpeg dmenu dunst \
-    jq xbacklight rofi  cmus 
+    libxcb-xrm0 libxcb-xrm-dev autoconf  xutils-dev libtool automake  libxcb-shape0-dev \
+    jq xbacklight rofi cmus ffmpeg dmenu dunst 
     
-    
-cd /tmp
+
+## install i3-gaps 
+cd tmp
 git clone https://www.github.com/Airblader/i3 i3-gaps
-cd /tmp/i3-gaps
+cd i3-gaps
+git checkout gaps && git pull
 autoreconf --force --install
-rm -rf /tmp/i3-gaps/build
-mkdir -p /tmp/i3-gaps/build
-cd /tmp/i3-gaps/build
+rm -rf build
+mkdir build
+cd build
 ../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
 make
-make install
-cd /home/nick/tools
-
+sudo make install
 
 
 
